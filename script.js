07002e1f5ea12077b1ba29ddc0dc54ed3ba30239
meddlingKids <rodneymiles@gmail.com>
2021-11-11 12:07:35 -0500

    4 files modified

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('hello!')
+console.log('hello! we have a full deck!')
+
+let deck = ["1","2","3","4","5","6","7","8","9","10","11","12","13"]
+
+
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+function shuffleArray(deck) {
+  for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = deck[i];
+      array[i] = deck[j];
+      array[j] = temp;
+  }
+}
+
+for (let i = 0; i < deck.length; i++) {
+  console.log(deck[i])
+  console.log('there are ' + deck.length + ' cards left in the deck')
+}
